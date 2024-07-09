c9c7c900dc9df8bf9b407209eae45ace89a1a06e
zohaibshahzad1990 <zohaibshazad1990@gmail.com>
2024-07-10 00:29:27 +0500

    https conversion

--- dofactory.react.js1/vite.config.js
+++ dofactory.react.js1/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(),mkcert()],
     server: {
         port: 5191,       // Assigned port number (default is 5173)
-        strictPort: true  // Because server CORS policy only accepts above port
+        strictPort: true,  // Because server CORS policy only accepts above port
+        https:true
     },
 })
